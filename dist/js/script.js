b09ea8107eea43cee0dc333a3392c83c6f386b4f
Musyaffak <musyaffaksyafak@gmail.com>
2024-09-28 14:22:48 +0700

    pembuatan dark mode & pebaikan fitur

--- dist/js/script.js
+++ dist/js/script.js
@@ -33,13 +33,26 @@ window.addEventListener("click", function (e) {
 });
 
 // Dark Mode Toggle
-const darkToggle = document.querySelector('#dark-toggle');
-const html = document.querySelector('html');
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
 
-darkToggle.addEventListener('click', function() {
+darkToggle.addEventListener("click", function () {
   if (darkToggle.checked) {
-    html.classList.add('dark');
+    html.classList.add("dark");
+    localStorage.theme = "dark";
   } else {
-    html.classList.remove('dark');
+    html.classList.remove("dark");
+    localStorage.theme = "light";
   }
 });
+
+// Pindah posisi toggle
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
